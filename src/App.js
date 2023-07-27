6faee8471b52142712a2ff60901da6560d076d2b
satisht <satisht@cdac.in>
2023-07-27 10:29:14 +0530

    added basic loading screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { AnimatePresence, motion } from 'framer-motion' 
+import { CircularProgress, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { useEffect, useState } from 'react';
+import CircularProg from './components/CircularProg';
 function App() {
+  const [state , setState] = useState(true);
+  useEffect(()=>{
+    setTimeout(()=>{
+      setState(false)
+    },3000)
+  })
   return (
-    <div className="App">
+    <>
+   
+    {!state && <motion.div className="App"
+    initial={{
+      y : '-100vh'
+    }}
+    animate={{
+      y : 0
+    }}
+    transition={{
+      type : 'tween',
+      duration : 0.5 ,
+      ease : 'easeOut',
+    }}
+    >
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +42,34 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </motion.div>}
+    <AnimatePresence>
+   { state && <motion.div
+    initial = {{
+      opacity : 0
+    }}
+    animate = {{
+    opacity : 1  
+    }}
+    exit={{
+      opacity : 0
+    }}
+    >
+      <Box
+      sx={{
+        display : 'flex',
+        justifyContent : 'center',
+        alignItems : 'center',
+mt : '20%'
+      }}
+      >
+        <Typography variant='h1'>Loading</Typography> 
+     <CircularProg/>
+      </Box>
+     
+    </motion.div> }
+    </AnimatePresence>
+    </>
   );
 }
 
---
+++ src/components/Side_nav.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Side_nav() {
+  return (
+    <div>Side_nav</div>
+  )
+}
+
+export default Side_nav
